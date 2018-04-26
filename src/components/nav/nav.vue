b0f03419dfd154266bc6948c63a9b854225a044c
shiningWolf <31480234+shiningWolf@users.noreply.github.com>
2018-04-26 21:08:57 +0800

    vue-cli搭建项目上传
    vue-cli搭建项目上传

---
+++ src/components/nav/nav.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="navline">
+    <h1 class="title">
+      <router-link :to="{ name: 'HomeView'}">
+        豆瓣
+      </router-link>
+    </h1>
+    <ul class="nav">
+    	<li>
+    		<router-link style="color: #2384E8" :to="{ name: 'MovieView'}">
+	          电影
+	        </router-link>
+    	</li>
+    	<li>
+    		<router-link style="color: #9F7860" :to="{ name: 'BookView'}">
+	          图书
+	        </router-link>
+    	</li>
+    	<li>
+    		<router-link style="color: #E4A813" :to="{ name: 'StatusView'}">
+	          广播
+	        </router-link>
+    	</li>
+    	<li>
+    		<router-link style="color: #2AB8CC" :to="{ name: 'GroupView'}">
+	          小组
+	        </router-link>
+    	</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'nav',
+  data(){
+    return {
+        
+    }
+  }
+}
+</script>
+
+<style lang="scss" scope>
+.navline{
+  overflow:hidden;
+}
+.nav{
+  display:inline-block;
+  height:1.6rem;
+  line-height: 1.6rem;
+}
+.title{
+  display:inline-block;
+  float:left;
+  margin-left:1rem;
+  line-height: 1.6rem;
+  color: #00b600;
+}
+</style>
---
+++ src/views/home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+     <db-nav></db-nav>
+  </div>
+</template>
+
+<script>
+import nav from '../components/nav/nav'
+export default {
+  name: 'App',
+  data(){
+    return {
+        
+    }
+  },
+  components:{
+    'db-nav':nav
+  }
+}
+</script>
+
+<style>
+
+</style>
